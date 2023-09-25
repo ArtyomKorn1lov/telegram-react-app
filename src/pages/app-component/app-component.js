6d064fb5a4447aaf13b7ee755f8e9b999628245c
Артём Корнилов <temka.kornilov@mail.ru>
2023-09-25 09:50:33 +0300

    Initial commit

---
+++ src/pages/app-component/app-component.js
@@ -0,0 +1,13 @@
+import './app-component.css';
+
+const AppComponent = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world</h1>
+      </header>
+    </div>
+  );
+}
+
+export default AppComponent;
